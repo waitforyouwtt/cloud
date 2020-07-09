9251ed768351c5be639a698e0636fccfba608d37
wb00001086 <abcd1234>
2020-07-09 19:31:32 +0800

    初次提交：创建项目

---
+++ eureka-server-center/src/test/java/com/yidiandain/EurekaServerCenterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yidiandain;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootTest
+class EurekaServerCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
